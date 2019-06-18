f65fcc112dfb6de31d06140ef08a4e9c94c6dbfb
奥田圭 <ryupa13@gmail.com>
2019-06-18 11:21:56 +0900

    Playerを追加、移動処理を追加

---
+++ SuperAniki/Assets/okuda/Script/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6f;
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, v, 0);
+        movement = movement.normalized * speed * Time.deltaTime;
+        transform.position = transform.position + movement;
+    }
+}

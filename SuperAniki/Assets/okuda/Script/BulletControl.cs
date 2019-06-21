a44f3ad988ad600ad79e67fbf16f59973e394165
奥田圭 <ryupa13@gmail.com>
2019-06-21 14:05:06 +0900

    Player移動と攻撃追加、弾追加、HP実装

---
+++ SuperAniki/Assets/okuda/Script/BulletControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+    public float speed = 9.0f;
+
+    Vector3 movement;
+    Renderer ren;
+    bool check = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ren = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.Set(1, 0, 0);
+        movement = movement.normalized * speed * Time.deltaTime;
+        transform.position = transform.position + movement;
+
+        if(!check && ren.isVisible)
+        {
+            check = true;
+        }
+        if(check && !ren.isVisible)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ SuperAniki/Assets/okuda/Script/PlayerAttack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+    public GameObject bullet;
+
+    float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.Z) && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        timer = 0f;
+
+        Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+    }
+}

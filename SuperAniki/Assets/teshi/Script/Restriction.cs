b6d94d691171c2ae8fdf229ee115e5d5b1f51cee
tehsi307 <nerunerunerune402@gmail.com>
2019-06-25 13:07:40 +0900

    プレイヤー移動制限

---
+++ SuperAniki/Assets/teshi/Script/Restriction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restriction : MonoBehaviour
+{
+    
+    GameObject Player;
+    GameObject wall_Left;
+    GameObject wall_Right;
+    GameObject wall_Top;
+    GameObject wall_Bottom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("player");
+        wall_Left = GameObject.Find("LeftWall");
+         wall_Right = GameObject.Find("RightWall");
+         wall_Top = GameObject.Find("TopWall");
+         wall_Bottom = GameObject.Find("BottomWall");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player.transform.position = (new Vector3(Mathf.Clamp(Player.transform.position.x, wall_Left.transform.position.x, wall_Right.transform.position.x),
+            Mathf.Clamp(Player.transform.position.y, wall_Bottom.transform.position.y, wall_Top.transform.position.y), Player.transform.position.z));
+    }
+}

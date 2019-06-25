4878df9b807a1acf6c8d8ccaeeccad953dfab18a
tehsi307 <nerunerunerune402@gmail.com>
2019-06-25 10:58:05 +0900

    コミット

---
+++ SuperAniki/Assets/miya/script/LookAt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.LookAt(target.transform.position);
+    }
+}
---
+++ SuperAniki/Assets/teshi/Script/EnemyShoot Diffusion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootDiffusion : MonoBehaviour
+{
+    public GameObject enemybulletPrefab;
+    private int timeCount = 0;
+    public float enemyspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCount += 1;
+
+        if (timeCount % 60 == 0)
+        {
+            //敵の弾の生成
+            GameObject enemybullet = Instantiate(enemybulletPrefab, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody enemybulletRb = enemybullet.GetComponent<Rigidbody>();
+            //弾の飛ばす方向を決める
+            enemybulletRb.AddForce(transform.forward * enemyspeed);
+            //〇秒後に弾の削除
+            Destroy(enemybullet, 15.0f);
+
+        }
+    }
+}
